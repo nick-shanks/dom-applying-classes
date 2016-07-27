d9eb15f379e77a73c6abf61aa629d9e7d18d8433
Your Name <nickshanks7@gmail.com>
2016-07-28 11:26:30 +1200

    added visible box

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -31,3 +32,7 @@ function three () {
   three.classList.add('purple')
 }
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0];
+  makeVisible.classList.add('visible')
+}
